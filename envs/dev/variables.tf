988a6fe4507ed5be245cba8293b07cc4055751d3
Gerard <gerard.eklu@yahoo.fr>
2026-03-05 02:28:58 -0500

    update

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,5 +1,12 @@
-variable "region" {}
-variable "vpc_cidr" {}
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
 variable "public_subnet_cidr" {
   type    = string
   default = "10.0.1.0/24"
