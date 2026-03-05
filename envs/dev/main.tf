6453fa60544f712ce68913aee15162505afc31a6
Gerard <gerard.eklu@yahoo.fr>
2026-03-05 02:58:10 -0500

    create the terraform destroy file

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,11 +1,11 @@
 terraform {
-  backend "s3" {
-    bucket         = "gerard-terraform-state-2026"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
+  # backend "s3" {
+  #   bucket         = "gerard-terraform-state-2026"
+  #   key            = "dev/terraform.tfstate"
+  #   region         = "us-east-1"
+  #   dynamodb_table = "terraform-locks"
+  #   encrypt        = true
+  # }
 }
 
 provider "aws" {
